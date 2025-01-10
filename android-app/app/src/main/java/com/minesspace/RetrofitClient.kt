2286c23cbd14ea41dc97168a39396363e2d787aa
hugodito <hugo.ditomaso83@gmail.com>
2025-01-10 11:30:47 +0100

    Revert "Pull"

---
+++ android-app/app/src/main/java/com/minesspace/RetrofitClient.kt
@@ -0,0 +1,18 @@
+package com.minesspace
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitClient {
+
+    private const val BASE_URL = "http://10.0.2.2:3000" // Utilisez 10.0.2.2 pour accéder à localhost depuis un émulateur Android
+
+    val apiService: ApiService by lazy {
+        val retrofit = Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        retrofit.create(ApiService::class.java)
+    }
+}
